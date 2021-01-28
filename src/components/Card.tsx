0cdecb8819cdd01db39d2b37f29527e55eb572d4
jjordann <lesjakjordan@gmail.com>
2021-01-28 07:31:13 +0100

    card hover

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 
+import cardStyles from "../style/card.module.scss"
+
 const Card = (props: any) => {
   const [state, setState] = useState(props);
 
   const marginLeft = state.id * 100;
 
   return (
-    <img src={process.env.PUBLIC_URL + '/cards/fmf/' + props.value + '.png'} style={{position: 'absolute', marginLeft: marginLeft}} alt={props.value} />
+    <img src={process.env.PUBLIC_URL + '/cards/fmf/' + props.value + '.png'} className={cardStyles.card}
+    
+    style={{position: 'absolute', marginLeft: marginLeft}} alt={props.value} />
   )
 }
 
