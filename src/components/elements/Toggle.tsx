0f695d3ff246fd9fb3540e860e2aacdfda7512fd
Tilen Gombač <tilen.gombac@gmail.com>
2021-01-30 01:43:55 +0100

    Add onChange handling to Toggle Element

--- src/components/elements/Toggle.tsx
+++ src/components/elements/Toggle.tsx
@@ -6,7 +6,8 @@ const Toggle = (props: any) => {
   return (
     <div className={toggleStyles.toggle}>
       <input type="checkbox" name={props.name} id={props.name}
-        className={toggleStyles.checkbox} />
+        className={toggleStyles.checkbox}
+          onChange={e => props.onChange(e.target.checked)} />
       <label className={toggleStyles.label} htmlFor={props.name}>
         <span className={toggleStyles.inner} />
         <span className={toggleStyles.switch} />
