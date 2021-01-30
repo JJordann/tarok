cd8f391dda4c80a86fe64ef450df6a8d28be245e
Tilen Gombač <tilen.gombac@gmail.com>
2021-01-30 21:39:27 +0100

    Reorder test cards

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 
 const Game = () => {
   const values = ['srce_kraljica', 'srce_kralj', 'kara_kralj', 'pik_kralj',
-                    'kriz_kralj', 'tarok_1', 'tarok_16', 'tarok_19', 'tarok_18',
+                    'kriz_kralj', 'tarok_1', 'tarok_16', 'tarok_18', 'tarok_19',
                       'tarok_20', 'tarok_21', 'tarok_22'];
 
   return (
