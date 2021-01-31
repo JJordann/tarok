9147f580cf78d2b6cbe61bb2995b89d59267e7cd
Tilen Gombač <tilen.gombac@gmail.com>
2021-01-31 04:44:32 +0100

    Refactor

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import cardStyles from "../style/card.module.scss"
+import getSocket from './global';
 
 const Card = (props: any) => {
-
-  let imgsrc = `${process.env.PUBLIC_URL}/cards/fmf/${props.value}.png`
+  let imgsrc = `${process.env.PUBLIC_URL}/cards/${props.cardPack}/${props.value}.png`
 
   return (
-    <div className={cardStyles.cardWrapper} style={{transform: `rotate(${props.rotation}deg)`}}>
+    <div className={cardStyles.cardWrapper}
+      style={{transform: `rotate(${props.rotation}deg)`}}
+        onClick={props.onClick}>
       <img src={imgsrc} className={cardStyles.card} alt={props.value} />
     </div>
   )
--- src/components/Hand.tsx
+++ src/components/Hand.tsx
@@ -5,10 +5,13 @@ import handStyles from "../style/hand.module.scss"
 
 const Hand = (props: any) => {
   const cards: JSX.Element[] = [];
+  const cardPack = 'fmf';
+
   for(let i = 0; i < props.size; i++) {
     // Rotate cards from -15deg to +15deg
     let rotation = -15 + (30 / props.size) * i;
-    cards.push(<Card value={props.values[i]} rotation={rotation} id={i} key={i} />);
+    cards.push(<Card value={props.values[i]} rotation={rotation}
+                            cardPack={cardPack} id={i} key={i} />);
   }
 
   return (
