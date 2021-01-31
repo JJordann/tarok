01769c6a6553c8713e30ce07ceb582935c076560
Tilen Gombač <tilen.gombac@gmail.com>
2021-02-01 00:53:38 +0100

    Remove semicolons

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import Hand from './Hand';
+import React, { useEffect, useState } from 'react'
+import Hand from './Hand'
 
-import { Link } from "react-router-dom"
-import getSocket from './global';
+import { Link } from 'react-router-dom'
+import getSocket from './global'
 
 const Game = () => {
-  const socket = getSocket();
+  const socket = getSocket()
 
   const [state, setState] = useState({
     table: [],     // cards on table
@@ -18,18 +18,18 @@ const Game = () => {
 
   useEffect(() => {
     // fetch connected users immediately after first render
-    socket.emit('getState');
+    socket.emit('getState')
 
     socket.on('getState', s => {
       console.log(JSON.parse(s))
       setState(JSON.parse(s))
-    });
+    })
 
 
     return () => {
       socket.off('getState')
     }
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
   
   return (
     <div>
@@ -38,4 +38,4 @@ const Game = () => {
   )
 }
 
-export default Game;
+export default Game
