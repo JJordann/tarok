b8ecfe4b1e3665142a78b42f9d6ecb196fde416f
Tilen Gombač <tilen.gombac@gmail.com>
2021-02-04 08:30:18 +0100

    Add pretty scrollbar

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,77 +1,84 @@
-import React, { useState, useEffect, useRef } from "react"
+import React, { useState, useEffect, useRef } from 'react'
+import { Scrollbars } from 'react-custom-scrollbars';
+
 import getSocket from './global'
 
 import chatStyles from '../style/chat.module.scss'
 
-import { Scrollbars } from 'react-custom-scrollbars';
 
+const Chat = () => {
 
-const Chat = ({ myName }) => {
+  let socket = getSocket()
 
-    let socket = getSocket()
+  let [history, setHistory] = useState([])
 
-    let _history = [{sender: "Lars Ulrich", message: "I'm sad"}, 
-                    {sender: "Dubioza Kolektiv", message: "Our music is for free"},
-                    {sender: "Dubioza Kolektiv", message: "You can download .mp3"}]
+  const historyRef = useRef(null)
 
-    let [history, setHistory] = useState(_history)
+  useEffect(
+    () => {
+      // register
+      socket.on('chat', msg => {
+        setHistory(old => old.concat(JSON.parse(msg)))
+      })
 
-    const historyRef = useRef(null)
+      socket.on('INFO', info => {
+        setHistory(old => old.concat({sender: 'INFO', message: info}))
+      })
 
-    useEffect(
-        () => {
-            // register
-            socket.on("chat", msg => {
-                setHistory(old => old.concat(JSON.parse(msg)))
-            })
+      return () => {
+        // cleanup
+        socket.off('chat')
+        socket.off('INFO')
+      }
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
-            socket.on("INFO", info => {
-                setHistory(old => old.concat({sender: 'INFO', message: info}))
-            })
-
-            return () => {
-                // cleanup
-                socket.off("chat")
-                socket.off("INFO")
-            }
-    }, []) // eslint-disable-line react-hooks/exhaustive-deps
-
-    useEffect(() => {
-        historyRef.current.scrollTop = historyRef.current.scrollHeight;
-    }, [history])
-
-
-
+  useEffect(() => {
+    historyRef.current.scrollTop = historyRef.current.scrollHeight;
+  }, [history])
     
-    const handleSend = (e) => {
-        e.preventDefault()
-        socket.emit("chat", mymessage)
-        setMymessage("")
+  const handleSend = (e) => {
+    e.preventDefault()
+    socket.emit('chat', mymessage)
+    setMymessage('')
+  }
+
+  const handleChange = e => {
+    console.log(mymessage)
+    setMymessage(e.target.value)
+  }
+
+  const [mymessage, setMymessage] = useState('')
+
+  let History = history.map(m => <div><span>{m['sender']}</span><p>{m['message']}</p></div>)
+
+  const renderThumb = ({ style, ...props }) => {
+    const thumbStyle = {
+      borderRadius: 6,
+      backgroundColor: 'rgba(0, 0, 0, 0.6)'
     }
-
-    const handleChange = e => {
-        console.log(mymessage)
-        setMymessage(e.target.value)
-    }
-
-    const [mymessage, setMymessage] = useState("")
-
-    let History = history.map(m => <div><span>{m["sender"]}</span><p>{m["message"]}</p></div>)
-
+    
     return (
-        <div className={chatStyles.wrapper}>
-            <div ref={historyRef} className={chatStyles.history}>
-                { History }
-            </div>
-            
-            <form className={chatStyles.inputRow} onSubmit={handleSend}>
-                <label>
-                    <input type="text" onChange={handleChange} value={mymessage} placeholder="Bodi glasen" />
-                </label>
-            </form>
-        </div>
+      <div style={{ ...style, ...thumbStyle}} {...props} />
     )
-}
+  }
+    
+  const CustomScrollbars = (props) => <Scrollbars renderThumbVertical={renderThumb} {...props} />
 
+  return (
+    <div className={chatStyles.wrapper}>
+      <CustomScrollbars autoHide autoHideTimeout={500} autoHideDuration={200}>
+        <div ref={historyRef} className={chatStyles.history}>
+          { History }
+        </div>
+      </CustomScrollbars>
+            
+      <form className={chatStyles.inputRow} onSubmit={handleSend}>
+        <label>
+          <input type="text" onChange={handleChange} value={mymessage} placeholder="Bodi glasen" />
+        </label>
+      </form>
+    </div>
+  )
+}
 
 export default Chat
