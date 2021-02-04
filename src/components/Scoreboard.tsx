89599415e826bd2de2b43d826f721640e5e717e9
Tilen Gombač <tilen.gombac@gmail.com>
2021-02-04 08:13:34 +0100

    Fix points scroll, add pretty scroll, add radelci

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -1,25 +1,76 @@
 import React from 'react';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import scoreboardStyle from '../style/scoreboard.module.scss';
 
 const Scoreboard = ({scores}) => {
 
-  // TODO: Rewrite as table so tbody is scrollable, not each one seperately
-  const Scores = scores.map(score => 
-    <div>
-      <h3>{score.name}</h3>
-      <div>
-        {score.points.map(point => 
-          <span>{point}</span>
-        )}
+  const renderThumb = ({ style, ...props }) => {
+    const thumbStyle = {
+      borderRadius: 6,
+      backgroundColor: 'rgba(0, 0, 0, 0.6)'
+    }
+
+    return (
+      <div style={{ ...style, ...thumbStyle}} {...props} />
+    )
+  }
+
+  const CustomScrollbars = (props) => <Scrollbars renderThumbVertical={renderThumb} {...props} />
+
+  const getScores = (scores) => {
+    const Radelci = scores.map(score => {
+      return (
+        <div className={scoreboardStyle.radelci}>
+          {[...Array((score.radelci ? score.radelci : 0))].map((element, index) => {
+            return (
+              <FontAwesomeIcon icon={faStar} />
+            )
+          })}
+        </div>
+      )
+    })
+
+    const Names = scores.map(score => <div className={scoreboardStyle.name}>{score.name}</div>)
+
+    const Points = scores.map(score => {
+      return (
+        <div className={scoreboardStyle.points}>
+          {score.points.map(point => <div className={scoreboardStyle.point}>{point}</div>)}
+        </div>
+      )
+    })
+
+    const Sums = scores.map(score => <div className={scoreboardStyle.sum}>{score.sum}</div>)
+
+    return (
+      <div className={scoreboardStyle.container}>
+        <div className={scoreboardStyle.radelciWrapper}>
+          {Radelci}
+        </div>
+
+        <div className={scoreboardStyle.namesWrapper}>
+          {Names}
+        </div>
+
+        <CustomScrollbars autoHide autoHideTimeout={500} autoHideDuration={200}>
+          <div className={scoreboardStyle.pointsWrapper}>
+            {Points}
+          </div>
+        </CustomScrollbars>
+
+        <div className={scoreboardStyle.sumsWrapper}>
+          {Sums}
+        </div>
       </div>
-      <h4>{score.sum}</h4>
-    </div>
-  )
+    )
+  }
 
   return (
     <div className={scoreboardStyle.scoreboard}>
-      {Scores}
+      {getScores(scores)}
     </div>
   )
 }
