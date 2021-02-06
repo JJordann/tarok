845c0f6e580e16d156695d03bbf922d9086a703c
Tilen Gombač <tilen.gombac@gmail.com>
2021-02-06 13:13:56 +0100

    Update contracts display text hide on not choosing

---
+++ src/util/GameTypes.tsx
@@ -0,0 +1,16 @@
+const GameTypes = {
+  'tri': 'Tri',
+  'dva': 'Dva',
+  'ena': 'Ena',
+  'solo_tri': 'Solo tri',
+  'solo_dva': 'Solo dva',
+  'solo_ena': 'Solo ena',
+  'solo_brez': 'Solo brez',
+  'pikolo': 'Pikolo',
+  'berac': 'Berač',
+  'odprti_berac': 'Odprti berač',
+  'klop': 'Klop',
+  'naprej': 'Naprej'
+}
+
+export default GameTypes
