717b5fcafebbe6ca7b36ec7677a143e8956ab167
jjordann <lesjakjordan@gmail.com>
2021-02-09 19:03:21 +0100

    test

--- src/api/game.py
+++ src/api/game.py
@@ -96,7 +96,7 @@ class Game:
                     "index": i,
                     "name": p["name"],
                     "sid": p["sid"],
-                    "hand": hands[i],
+                    "hand": hands[i][0:2],
                     "ready": True,
                     "cardsWon": [],
                     "contractBonus": [],
