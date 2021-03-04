5b6c72144bc03b3e7dc07dd511f1bad6beb5b942
Tilen Gombač <tilen.gombac@gmail.com>
2021-03-04 03:44:58 +0100

    Add RoundEnd, Table

---
+++ src/domain/GameScreen/Activity/RoundEnd/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import roundEndStyle from './style.module.scss'
+
+const RoundEnd = ({ results }) => {
+
+  return (
+    <div className={roundEndStyle.wrapper}>
+
+    </div>
+  )
+}
+
+export default RoundEnd
---
+++ src/domain/GameScreen/Activity/Table/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import tableStyle from './style.module.scss'
+
+const Table = ({ cards }) => {
+
+  return (
+    <div className={tableStyle.wrapper}>
+
+    </div>
+  )
+}
+
+export default Table
