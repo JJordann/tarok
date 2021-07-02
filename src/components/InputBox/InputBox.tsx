296af39d60cfb56df7c825d258061ae2be6f0c88
Tilen Gombač <tilen.gombac@gmail.com>
2021-07-02 21:48:38 +0200

    Refactor NameForm into InputBox component

---
+++ src/components/InputBox/InputBox.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+import inputBoxStyles from './InputBox.module.scss'
+
+const InputBox = ({initialValue = '', placeholder = '', onChange = undefined,
+                    onSubmit = undefined, minLength = 1}) => {
+
+  const [value, setValue] = useState(initialValue)
+
+  const handleChange = (e) => {
+    const input = e.target.value
+
+    setValue(input)
+
+    if(onChange !== undefined) {
+      onChange(input)
+    }
+  }
+
+  const handleSubmit = () => {
+    if(onSubmit !== undefined) {
+      onSubmit(value)
+    }
+  }
+
+  const handleKeyDown = (e) => {
+    if(e.key === 'Enter') {
+      handleSubmit()
+    }
+  }
+
+  return (
+    <div className={inputBoxStyles.container}>
+      <input type='text' placeholder={placeholder} value={value}
+          onChange={handleChange} onKeyDown={handleKeyDown} />
+
+        <div onClick={handleSubmit} className={(value && value.length >= minLength) ?
+            inputBoxStyles.active : inputBoxStyles.inactive}>
+        </div>
+    </div>
+  )
+}
+
+export default InputBox
--- src/components/Pages/LoginPage/LoginPage.tsx
+++ src/components/Pages/LoginPage/LoginPage.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 
-import NameForm from '../../NameForm/NameForm'
 import Header from '../../Header/Header'
+import InputBox from '../../InputBox/InputBox'
+
+import { getUser, setUser } from '../../../services/User/User'
+import { LOBBY_LIST_ROUTE } from '../../../routes'
 
 import loginPageStyles from './LoginPage.module.scss'
 
 const LoginPage = () => {
+  const history = useHistory()
+
+  if(getUser()) {
+    history.push(LOBBY_LIST_ROUTE)
+  }
+
+  const saveName = (name) => {
+    setUser(name)
+
+    history.push(LOBBY_LIST_ROUTE)
+  }
 
   return (
     <div className={loginPageStyles.wrapper}>
@@ -13,7 +28,7 @@ const LoginPage = () => {
       <div className={loginPageStyles.container}>
         <h1><span>Prijava</span></h1>
 
-        <NameForm />
+        <InputBox placeholder='Vnesi ime' onSubmit={saveName} minLength={3} />
       </div>
     </div>
   )
