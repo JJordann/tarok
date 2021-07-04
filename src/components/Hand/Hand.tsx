06ba42f03d6032e57ebda6ef70b1230014119af2
Tilen Gombač <tilen.gombac@gmail.com>
2021-07-04 22:48:15 +0200

    sort cards in hand

--- src/components/Hand/Hand.tsx
+++ src/components/Hand/Hand.tsx
@@ -4,6 +4,23 @@ import Card from './Card'
 
 import handStyles from './Hand.module.scss'
 
+const cardsOrdered = [
+  'kara_4', 'kara_3', 'kara_2', 'kara_1',
+  'kara_poba', 'kara_konj', 'kara_kraljica', 'kara_kralj',
+  'kriz_7', 'kriz_8', 'kriz_9', 'kriz_10',
+  'kriz_poba', 'kriz_konj', 'kriz_kraljica', 'kriz_kralj',
+  'pik_7', 'pik_8', 'pik_9', 'pik_10',
+  'pik_poba', 'pik_konj', 'pik_kraljica', 'pik_kralj',
+  'srce_4', 'srce_3', 'srce_2', 'srce_1',
+  'srce_poba', 'srce_konj', 'srce_kraljica', 'srce_kralj',
+  'tarok_1', 'tarok_2', 'tarok_3', 'tarok_4',
+  'tarok_5', 'tarok_6', 'tarok_7', 'tarok_8',
+  'tarok_9', 'tarok_10', 'tarok_11', 'tarok_12',
+  'tarok_13', 'tarok_14', 'tarok_15', 'tarok_16',
+  'tarok_17', 'tarok_18', 'tarok_19', 'tarok_20',
+  'tarok_21', 'tarok_22'
+];
+
 const Hand = ({cards}) => {
 
   // To "bend" the cards as one would in their physical hands
@@ -11,7 +28,19 @@ const Hand = ({cards}) => {
   const cardTranslation = (index, length) =>
     -(Math.pow(((index - length) / 2), 2)) * 24 / length - 60;
 
-  const Hand = cards.map((card, index) =>
+  const sortedCards = () => {
+    cards.sort(compareCardOrder)
+
+    return cards
+  }
+
+  const compareCardOrder = (a, b) => {
+    if(cardsOrdered.indexOf(a) < cardsOrdered.indexOf(b)) return -1
+
+    return 1
+  }
+
+  const Hand = sortedCards().map((card, index) =>
   //
     <div className={handStyles.cardContainer}
       style={{transform: `rotate(${cardRotation(index, cards.length)}deg)`,
