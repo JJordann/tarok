9fe0fdcc57ae8fd49ce262ee1ec520aa3e42125b
Tilen Gombač <tilen.gombac@gmail.com>
2021-10-10 16:28:57 +0200

    Update dependencies

--- src/services/Socket/socket.ts
+++ src/services/Socket/socket.ts
@@ -1,5 +1,5 @@
-import socketio from 'socket.io-client'
+import { io } from 'socket.io-client'
 
 import { SOCKET_URL } from './config'
 
-export const socket = socketio.connect(SOCKET_URL)
+export const socket = io(SOCKET_URL)
