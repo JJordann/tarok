c5f3db86b33450a379249352bda8401c185977b0
Tilen Gombač <tilen.gombac@gmail.com>
2022-05-22 13:40:36 +0200

    Clean-ish start
    Update dependencies, replace node-sass with sass, remove most components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react'
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import { GAME_ROUTE, HOME_ROUTE, LOBBY_LIST_ROUTE, LOGIN_ROUTE } from '../../routes'
-
-import GamePage from '../Pages/GamePage'
-import LandingPage from '../Pages/LandingPage'
-import LobbyListPage from '../Pages/LobbyListPage'
-import Error404Page from '../Pages/Error404Page'
-import LoginPage from '../Pages/LoginPage'
+import { HOME_ROUTE } from '../../routes'
+import Hand from '../Hand/Hand'
 
 const App = () => {
 
   return (
     <BrowserRouter>
-      <Switch>
-          <Route path={HOME_ROUTE} exact component={LandingPage} />
-          <Route path={LOBBY_LIST_ROUTE} exact component={LobbyListPage} />
-          <Route path={GAME_ROUTE} exact component={GamePage} />
-          <Route path={LOGIN_ROUTE} exact component={LoginPage} />
-          <Route component={Error404Page} />
-      </Switch>
+      <Routes>
+          <Route path={HOME_ROUTE} element={<Hand cards={['tarok_2']} />} />
+      </Routes>
     </BrowserRouter>
   )
 }
